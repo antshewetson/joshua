dbb1edde0585cab88040294fe885f1d74dbb2203
Ants <antshewetson@gmail.com>
2018-10-24 18:08:20 +0100

    Added C# version

---
+++ ASteroidsCS/ASteroidsCS/Form1.Designer.cs
@@ -0,0 +1,181 @@
+﻿namespace ASteroidsCS
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.Label3 = new System.Windows.Forms.Label();
+            this.GameTick = new System.Windows.Forms.Timer(this.components);
+            this.Label9 = new System.Windows.Forms.Label();
+            this.Label4 = new System.Windows.Forms.Label();
+            this.Label2 = new System.Windows.Forms.Label();
+            this.Label1 = new System.Windows.Forms.Label();
+            this.Label8 = new System.Windows.Forms.Label();
+            this.Label7 = new System.Windows.Forms.Label();
+            this.Label6 = new System.Windows.Forms.Label();
+            this.Label5 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // Label3
+            // 
+            this.Label3.AutoSize = true;
+            this.Label3.Location = new System.Drawing.Point(707, 428);
+            this.Label3.Name = "Label3";
+            this.Label3.Size = new System.Drawing.Size(81, 13);
+            this.Label3.TabIndex = 3;
+            this.Label3.Text = "WASD to move";
+            // 
+            // GameTick
+            // 
+            this.GameTick.Enabled = true;
+            this.GameTick.Interval = 20;
+            this.GameTick.Tick += new System.EventHandler(this.GameTick_Tick);
+            // 
+            // Label9
+            // 
+            this.Label9.AutoSize = true;
+            this.Label9.Location = new System.Drawing.Point(19, 64);
+            this.Label9.Name = "Label9";
+            this.Label9.Size = new System.Drawing.Size(39, 13);
+            this.Label9.TabIndex = 19;
+            this.Label9.Text = "Label9";
+            // 
+            // Label4
+            // 
+            this.Label4.AutoSize = true;
+            this.Label4.Location = new System.Drawing.Point(13, 47);
+            this.Label4.Name = "Label4";
+            this.Label4.Size = new System.Drawing.Size(39, 13);
+            this.Label4.TabIndex = 18;
+            this.Label4.Text = "Label4";
+            // 
+            // Label2
+            // 
+            this.Label2.AutoSize = true;
+            this.Label2.Location = new System.Drawing.Point(16, 30);
+            this.Label2.Name = "Label2";
+            this.Label2.Size = new System.Drawing.Size(39, 13);
+            this.Label2.TabIndex = 17;
+            this.Label2.Text = "Label2";
+            // 
+            // Label1
+            // 
+            this.Label1.AutoSize = true;
+            this.Label1.Location = new System.Drawing.Point(13, 13);
+            this.Label1.Name = "Label1";
+            this.Label1.Size = new System.Drawing.Size(39, 13);
+            this.Label1.TabIndex = 16;
+            this.Label1.Text = "Label1";
+            // 
+            // Label8
+            // 
+            this.Label8.AutoSize = true;
+            this.Label8.Location = new System.Drawing.Point(749, 64);
+            this.Label8.Name = "Label8";
+            this.Label8.Size = new System.Drawing.Size(39, 13);
+            this.Label8.TabIndex = 23;
+            this.Label8.Text = "Label8";
+            // 
+            // Label7
+            // 
+            this.Label7.AutoSize = true;
+            this.Label7.Location = new System.Drawing.Point(749, 47);
+            this.Label7.Name = "Label7";
+            this.Label7.Size = new System.Drawing.Size(39, 13);
+            this.Label7.TabIndex = 22;
+            this.Label7.Text = "Label7";
+            // 
+            // Label6
+            // 
+            this.Label6.AutoSize = true;
+            this.Label6.Location = new System.Drawing.Point(748, 30);
+            this.Label6.Name = "Label6";
+            this.Label6.Size = new System.Drawing.Size(39, 13);
+            this.Label6.TabIndex = 21;
+            this.Label6.Text = "Label6";
+            // 
+            // Label5
+            // 
+            this.Label5.AutoSize = true;
+            this.Label5.Location = new System.Drawing.Point(748, 13);
+            this.Label5.Name = "Label5";
+            this.Label5.Size = new System.Drawing.Size(39, 13);
+            this.Label5.TabIndex = 20;
+            this.Label5.Text = "Label5";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(11, 428);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(41, 13);
+            this.label10.TabIndex = 24;
+            this.label10.Text = "label10";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.Label8);
+            this.Controls.Add(this.Label7);
+            this.Controls.Add(this.Label6);
+            this.Controls.Add(this.Label5);
+            this.Controls.Add(this.Label9);
+            this.Controls.Add(this.Label4);
+            this.Controls.Add(this.Label2);
+            this.Controls.Add(this.Label1);
+            this.Controls.Add(this.Label3);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyUp);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        internal System.Windows.Forms.Label Label3;
+        internal System.Windows.Forms.Timer GameTick;
+        internal System.Windows.Forms.Label Label9;
+        internal System.Windows.Forms.Label Label4;
+        internal System.Windows.Forms.Label Label2;
+        internal System.Windows.Forms.Label Label1;
+        internal System.Windows.Forms.Label Label8;
+        internal System.Windows.Forms.Label Label7;
+        internal System.Windows.Forms.Label Label6;
+        internal System.Windows.Forms.Label Label5;
+        internal System.Windows.Forms.Label label10;
+    }
+}
+
---
+++ ASteroidsCS/ASteroidsCS/Form1.cs
@@ -0,0 +1,265 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ASteroidsCS
+{
+    public partial class Form1 : Form
+    {
+        //This is the x and y of the players "centre" point where all calculations are based off of
+        int x = 50;
+        int y = 50;
+
+        //used to determine tick rate
+        static decimal fps = 60;
+
+        //Variables for detecting key presses
+        bool wpress;
+        bool apress;
+        bool spress;
+        bool dpress;
+
+        //misc maths variables
+        int r = 0;        //Roataion from ghost image
+        int rv = 5;       //Rotation multiplier
+        //decimal v = 0;    //Velocity
+        //decimal maxv = 7; //Max velocity
+        // AHH: I changed these to doubles to prevent having to do frequent 
+        //      casting gymnastics and just do the cast at the last moment
+        double v = 0;    //Velocity
+        double maxv = 7; //Max velocity
+
+        int length = 20; //Length of player
+
+        //Drawing stuff
+        Graphics formGraphics; //Graphics layer?
+        Pen playerpen = new Pen(Color.Blue, 1); //Blue pen
+        Pen debugpen = new Pen(Color.Red, 1); //Red pen
+        Pen debugpen2 = new Pen(Color.Green, 1); //Green pen
+        
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //Load graphics
+            formGraphics = CreateGraphics();
+
+            //Makes the tickspeed relevant to set fps
+            GameTick.Interval = (int)(1 / (fps / 1000));
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            //Getting key presses and storing htem in the respective variables
+            switch (e.KeyCode)
+            {
+                case Keys.W:
+                    wpress = true;
+                    break;
+                case Keys.A:
+                    apress = true;
+                    break;
+                case Keys.S:
+                    spress = true;
+                    break;
+                case Keys.D:
+                    dpress = true;
+                    break;
+            }
+            
+        }
+
+        private void Form1_KeyUp(object sender, KeyEventArgs e)
+        {
+            //Getting key presses and storing htem in the respective variables
+            switch (e.KeyCode)
+            {
+                case Keys.W:
+                    wpress = false;
+                    break;
+                case Keys.A:
+                    apress = false;
+                    break;
+                case Keys.S:
+                    spress = false;
+                    break;
+                case Keys.D:
+                    dpress = false;
+                    break;
+            }
+        }
+
+        private void GameTick_Tick(object sender, EventArgs e)
+        {
+            //Radians stuff
+            double ToRadians = Math.PI / 180;
+            double ToDegrees = 180 / System.Math.PI;
+            double rr;
+            //Rotation direction
+            int rdir;
+
+            //'Checking w and s
+            //If wpress = True And spress = True Then
+            //    'Nothing
+            //ElseIf wpress = True Then
+            //    v = v + 0.1
+            //ElseIf spress = True Then
+            //    v = v - 0.1
+            //End If
+            if (wpress == true && spress == true)
+            {
+                //Nothing
+            }
+            else if (wpress == true)
+            {
+                //v = v + (decimal)(0.1); // See comment at the declaration of v
+                v = v + 0.1;
+            }
+            else if (spress == true)
+            {
+                v = v - 0.1;
+            }
+
+            //Keeping v in acceptable values
+            //If v > maxv Then v = maxv
+            //If v< 0 Then v = 0
+            if (v > maxv) v = maxv;
+            if (v < 0) v = 0;
+            
+            //Checking a and d
+            //If dpress = True And apress = True Then
+            //    rdir = 0
+            //ElseIf dpress = True Then
+            //    rdir = -1
+            //ElseIf apress = True Then
+            //    rdir = 1
+            //Else
+            //    rdir = 0
+            //End If
+            if (dpress == true && apress == true)
+            {
+                rdir = 0;
+            }
+            else if (dpress == true)
+            {
+                rdir = -1;
+            }
+            else if (apress == true)
+            {
+                rdir = 1;
+            }
+            else
+            {
+                rdir = 0;
+            }
+
+            //Updating rotation value
+            //r = r + rdir * rv
+            r = r + rdir * rv;
+
+            //Keeping r in acceptable values
+            //If r > 360 Then r = r - 360
+            //If r< 0 Then r = r + 360
+            if (r > 360) r = r - 360;
+            if (r < 360) r = r + 360;
+
+            //Making radians r
+            //rr = r * ToRadians
+            rr = r * ToRadians;
+
+            //Different points used for drawing
+            //Dim forward As Point
+            //Dim back As Point
+            //Dim centre As Point
+            //Dim p1 As Point 'These two are used for the left and right point to make the shape
+            //Dim p2 As Point
+            Point forward;
+            Point back;
+            Point centre;
+            Point p1; //These two are used for the left and right point to make the shape
+            Point p2;
+
+            //formGraphics.Clear(Color.White)
+            formGraphics.Clear(Color.White);
+
+            //Updating centrepint using v and r
+            //x = x + v * Math.Cos(90 * ToRadians - rr)
+            //y = y + v * Math.Sin(90 * ToRadians - rr)
+            x = (int)(x + v * Math.Cos((90 * ToRadians) - rr));
+            y = (int)(y + v * Math.Sin((90 * ToRadians) - rr));
+
+            //Updating labels
+            //Label5.Text = wpress
+            //Label6.Text = apress
+            //Label7.Text = spress
+            //Label8.Text = dpress
+            //Label4.Text = r
+            //Label1.Text = x
+            //Label2.Text = y
+            Label5.Text = wpress.ToString();
+            Label6.Text = apress.ToString();
+            Label7.Text = spress.ToString();
+            Label8.Text = dpress.ToString();
+            Label4.Text = r.ToString();
+            Label1.Text = x.ToString();
+            Label2.Text = y.ToString();
+
+
+            label10.Text = v.ToString();
+
+
+            //'Setting the centre
+            //centre.X = x
+            //centre.Y = y
+            centre = new Point(x, y);
+
+            //'Setting thetopand bottom points of the shape
+            //forward.X = x + (0.5 * length * Math.Sin(rr))
+            //forward.Y = y + (0.5 * length * Math.Cos(rr))
+            //back.X = x - (0.5 * length * Math.Sin(rr))
+            //back.Y = y - (0.5 * length * Math.Cos(rr))
+            forward = new Point(
+                (int)(x + (0.5 * length * Math.Sin(rr))), 
+                (int)(y + (0.5 * length * Math.Cos(rr))));
+            back = new Point(
+                (int)(x - (0.5 * length * Math.Sin(rr))), 
+                (int)(y - (0.5 * length * Math.Cos(rr))));
+
+            //Setting the two edge points
+            //p1.X = x + length * Math.Sin(rr + 160 * ToRadians)
+            //p1.Y = y + length * Math.Cos(rr + 160 * ToRadians)
+            //p2.X = x + length * Math.Sin(rr + 200 * ToRadians)
+            //p2.Y = y + length * Math.Cos(rr + 200 * ToRadians)
+            p1 = new Point(
+                (int)(x + length * Math.Sin(rr + 160 * ToRadians)), 
+                (int)(y + length * Math.Cos(rr + 160 * ToRadians)));
+            p2 = new Point(
+                (int)(x + length * Math.Sin(rr + 200 * ToRadians)),
+                (int)(y + length * Math.Cos(rr + 200 * ToRadians)));
+
+            //Drawing the shape
+            //formGraphics.DrawLine(playerpen, forward, p1)
+            //formGraphics.DrawLine(playerpen, p1, back)
+            //formGraphics.DrawLine(playerpen, forward, p2)
+            //formGraphics.DrawLine(playerpen, p2, back)
+            formGraphics.DrawLine(playerpen, forward, p1);
+            formGraphics.DrawLine(playerpen, p1, back);
+            formGraphics.DrawLine(playerpen, forward, p2);
+            formGraphics.DrawLine(playerpen, p2, back);
+
+
+            //Debug line so i can see the shape if i muck up
+            //formGraphics.DrawLine(debugpen, forward, back)
+            formGraphics.DrawLine(debugpen, forward, back);
+        }
+    }
+}
